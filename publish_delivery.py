119c65f9b3519f4c55fcea02492f909427015d12
Ned Wilson <ned@qppe.tech>
2019-01-22 18:56:38 -0800

    Add --playlistonly command line argument, to only build a playlist and do nothing else.

--- publish_delivery.py
+++ publish_delivery.py
@@ -11,6 +11,7 @@ parser.add_argument('--interactive', help='Will prompt the user for version remo
 parser.add_argument('--noemail', help='Will not sync or send email', action='store_true')
 parser.add_argument('--matte', help='Creates a matte delivery', action='store_true')
 parser.add_argument('--combined', help='Delivers both high resolution and movie files', action='store_true')
+parser.add_argument('--playlistonly', help='Only creates a playlist in the database, does not actually perform the submission or copy data', action='store_true')
 args = parser.parse_args()
 
 b_gui = False
@@ -19,6 +20,7 @@ b_interactive = False
 b_email = True
 b_matte = False
 b_combined = False
+b_playlistonly = False
 
 if args.gui:
     b_gui = True
@@ -42,6 +44,10 @@ if args.combined:
     b_combined = True
     print "INFO: Setting combined to true - will include both movie files and high resolution images in this package."
 
+if args.playlistonly:
+    b_playlistonly = True
+    print "INFO: Setting playlist only to true - will only build a playlist for this submission, will not copy data or actually build the submisson."
+
 if args.matte:
     b_matte = True
     b_hires = False
@@ -50,8 +56,8 @@ if args.matte:
 delivery.globals_from_config()
 
 if b_gui:
-    delivery.display_window(m_2k=b_hires, send_email=b_email, m_matte=b_matte, m_combined=b_combined)
+    delivery.display_window(m_2k=b_hires, send_email=b_email, m_matte=b_matte, m_combined=b_combined, m_playlistonly=b_playlistonly)
 else:
-    delivery.execute_shell(m_interactive=b_interactive, m_2k=b_hires, send_email=b_email, m_matte=b_matte, m_combined=b_combined)
+    delivery.execute_shell(m_interactive=b_interactive, m_2k=b_hires, send_email=b_email, m_matte=b_matte, m_combined=b_combined, m_playlistonly=b_playlistonly)
 
     
